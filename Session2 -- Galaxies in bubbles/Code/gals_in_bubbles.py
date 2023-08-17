b849036a1465ae457b401834269f89c7dca272a1
Benjamin Andrew Metha <methab@student.unimelb.edu.au>
2023-08-17 15:05:58 +1000

    Create a nonfunctional version of this program...

--- Session2 -- Galaxies in bubbles/Code/gals_in_bubbles.py
+++ Session2 -- Galaxies in bubbles/Code/gals_in_bubbles.py
@@ -7,9 +7,37 @@ Out: How many galaxies are contained by each bubble?
 
 
 import numpy as np
-from sklearn.pairwise import euclidean_distances
+from astropy.io import ascii
+from sklearn.metrics.pairwise import euclidean_distances 
 
+REDSHIFT = 10
+
+def read_data():
+	'''
+	Open Manu's data file.
+	
+	Parameters
+	---------
+	
+	Returns
+	-------
+	
+	the data file, opened and cleaned
+	'''
+	tab = ascii.read('../Data/GalMeraxes.dat')
+	return tab
+	
 
 if __name__ == '__main__':
-	df = read_data()
+	tab = read_data()
+	x = tab['col2']
+	y = tab['col3']
+	z = tab['col4']
+	r = tab['col5'] * (1+REDSHIFT) # converting physical to comoving parsecs
+	
+	# stack coordinates together
+	coords = np.vstack([x,y,z]).T
+	
+	# Find all distances between all galaxies
+	dist_matrix = euclidean_distances(coords, coords)
 	

46a9a2550e4cf8cb1976f6747b2d8f171b912d8f
Benjamin Andrew Metha <methab@student.unimelb.edu.au>
2023-08-17 15:37:18 +1000

    Try to restructure it to go row by row

--- Session2 -- Galaxies in bubbles/Code/gals_in_bubbles.py
+++ Session2 -- Galaxies in bubbles/Code/gals_in_bubbles.py
@@ -7,17 +7,17 @@ Out: How many galaxies are contained by each bubble?
 
 
 import numpy as np
-from astropy.io import ascii
 from sklearn.metrics.pairwise import euclidean_distances 
 
 REDSHIFT = 10
 
-def read_data():
+def read_data(n_rows = None):
 	'''
 	Open Manu's data file.
 	
 	Parameters
 	---------
+	n_rows: only return the first n_rows
 	
 	Returns
 	-------
@@ -25,19 +25,27 @@ def read_data():
 	the data file, opened and cleaned
 	'''
 	tab = ascii.read('../Data/GalMeraxes.dat')
-	return tab
-	
+	if n_rows is None:
+		return tab
+	else:
+		return tab[:n_rows]
 
 if __name__ == '__main__':
-	tab = read_data()
+	tab = read_data())
 	x = tab['col2']
 	y = tab['col3']
 	z = tab['col4']
 	r = tab['col5'] * (1+REDSHIFT) # converting physical to comoving parsecs
 	
-	# stack coordinates together
-	coords = np.vstack([x,y,z]).T
+	# stack all coordinates together
+	all_coords = np.vstack([x,y,z]).T
 	
-	# Find all distances between all galaxies
-	dist_matrix = euclidean_distances(coords, coords)
+	n_gals = len(x)
+	n_in_bubble = np.empty(n_gals)
+	for ii in range(n_gals):
+		gal_coords = np.array(x[ii], y[ii], z[ii])
+		# Find all distances between all galaxies
+		dist_to_gal = euclidean_distances(gal_coords, all_coords)
+	
+		n_in_bubble[ii] = np.sum(dist_to_gal < r[ii])
 	
